ac545c938d45fa4de9be8695ef578f57df44ae8a
LongWang <476403337@qq.com>
2019-02-09 10:34:34 +0800

    android base

---
+++ android/SupBank/app/src/main/java/supinfo/com/supbank/activity/StartActivity.java
@@ -0,0 +1,15 @@
+package supinfo.com.supbank.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import supinfo.com.supbank.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
