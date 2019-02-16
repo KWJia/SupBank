89fa3276bbe0218c22f8c71e7496527830edf5b1
XIE Hui <hui.x@outlook.com>
2019-02-17 00:32:19 +0800

    new webapp by vue & typescript

---
+++ web/src/router.ts
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        title: 'SupBank',
+      },
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Signin.vue'),
+      meta: {
+        title: 'Sign In',
+      },
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Signup.vue'),
+      meta: {
+        title: 'Sign Up',
+      },
+    },
+  ],
+});
