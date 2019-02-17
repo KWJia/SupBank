e30987ae4b3d38cfd2c5b9f2882579f61c919611
XIE Hui <hui.x@outlook.com>
2019-02-17 21:49:59 +0800

    feat(table): add home & search pages’ transaction list

--- web/src/router.ts
+++ web/src/router.ts
@@ -54,9 +54,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Search.vue'),
-      props: (route) => ({
-        query: route.query.q,
-      }),
       meta: {
         title: 'Search',
       },
--- web/src/views/Search.vue
+++ web/src/views/Search.vue
@@ -1,38 +1,78 @@
 <template>
-  <div class="search"></div>
+  <div class="search">
+    <div class="transactionListCard card">
+      <table class="listTable">
+        <caption>transaction</caption>
+        <tr>
+          <th>Height</th>
+          <th>Hash</th>
+          <th>Amount</th>
+          <th>Time</th>
+        </tr>
+        <tr v-for="(t, k, i) in searchResultList">
+          <div v-if="i <= 6">
+            <td>t.height</td>
+            <td>t.transactionhash</td>
+            <td>t.amount</td>
+            <td>t.timestamp</td>
+          </div>
+        </tr>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios';
 
 @Component
 export default class Search extends Vue {
-  @Prop() private q!: string;
-
   private data() {
     return {
-      searchText: this.q,
+      searchText: this.$route.query.q,
+      searchResultList: [],
+      pages: 1,
+      page: 1,
     };
   }
 
   private created() {
-    const token = localStorage.getItem('token');
-    const xmlhttp = new XMLHttpRequest();
+    const searchText = this.$route.query.q;
 
-    xmlhttp.open('GET', '//localhost:3000/api/searchTransaction?searchtext=' + this.q, true);
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = (): void => {
-      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-        const response = JSON.parse(xmlhttp.responseText);
-
-        if (response.ack === 'success') {
-          alert(response.data.searchResultList);
-        } else {
-          alert(response.data.msg);
-        }
+    axios.get('//localhost:3000/api/searchTransaction', {
+      params: {
+        searchtext: searchText,
+      },
+    }).then((response) => {
+      const data = response.data;
+      if (data.ack === 'success') {
+        this.$data.searchResultList = data.data.searchResultList;
+      } else {
+        alert(data.data.msg);
       }
-    };
+    }).catch((error) => {
+      alert(error);
+    });
   }
 }
 </script>
+
+<style scoped>
+.searchCard .desc {
+  margin-top: 18px;
+  margin-bottom: 30px;
+  font-size: 18px;
+  color: #8590a6;
+}
+
+.listTable {
+  display: flex;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  justify-content: center;
+}
+</style>
